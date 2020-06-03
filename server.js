ae66ced08a35bc5f8b1e1e334dc92c376284192c
Tyler Buck <57814333+thetylerb@users.noreply.github.com>
2020-06-03 15:46:08 -0400

    Create server.js

---
+++ server.js
@@ -0,0 +1,89 @@
+const express = require('express');
+const mongoose = require('mongoose');
+const morgan = require('morgan');
+const bodyParser = require('body-parser');
+const path = require('path');
+const db = require('./models')
+const PORT = process.env.PORT || 3001;
+
+const app = express();
+
+app.use(morgan('dev'));
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.use(express.static('public'));
+
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost/workouts', {
+    useNewUrlParser: true,
+    useFindAndModify: false
+});
+
+app.get('/exercise', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/exercise.html'));
+});
+
+app.get('/stats', function(req, res) {
+    res.sendFile(path.join(__dirname, './public/stats.html'));
+});
+
+app.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "./public/index.html"));
+});
+
+app.get('/api/workouts', (req, res) => {
+    db.Workout.find({}).then(Workout => {
+        res.json(Workout);
+    })
+    .catch(error => {
+        res.json(error);
+    });
+});
+
+app.get('/api/wourkouts/:id', (req, res) => {
+    db.Workout.findOne({_id: req.params.id}).then(Workout => {
+        res.json(Workout);
+    })
+    .catch(error => {
+        res.json(error)
+    })
+})
+
+app.get('/api/workouts/range', (req, res) => {
+    db.Workout.find({}).then(Workout => {
+        res.json(Workout);
+    })
+    .catch(error => {
+        res.json(error);
+    });
+});
+
+app.post("/api/workouts", ({ body }, res)=> {
+    body.day = new Date(),
+    body.exercises = []
+    db.Workout.create(body).then(Workout => {
+        res.json(Workout);
+    })
+    .catch(error => {
+        res.json(error);
+    });
+});
+
+app.put('api/workouts/:id', (req, res) => {
+    let data = req.body;
+    db.Workout.update(
+        {_id: req.params.id},
+        {$push: { exercises: data }},
+        (error, response) => {
+            res.json(response);
+        }
+    )
+})
+
+app.listen(PORT, () => {
+    console.log(`App is listening on port ${PORT}`);
+});
